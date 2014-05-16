39e5b72c4b78caef15d484e4c747931e189f2589
emma <emma.nash@pebblecode.com>
2014-05-16 17:30:06 +0100

    Refactors to finding all the rearrangements of a qualifying sequence to a juggling sequence. Algorithm is no longer wrapped up in a type.

--- Juggling/QualifyingToAllJugglingRearrangements.fs
+++ Juggling/QualifyingToAllJugglingRearrangements.fs
@@ -5,17 +5,13 @@
     open ModularArithmetic
     open BasicJugglingFunctions
 
-    type private QualifyingSequenceWithFindAllJugglingRearrangementsCapability (inputSequence) =
-        
-        let p = List.length inputSequence
-        let moduloP = ModuloP p
-        let (+~) = moduloP.AddModP ()
+    let private listRemoveFirst element =
+        let folder (xs, state) x = if state && x = element then xs, not state
+                                    else x :: xs, state
+        List.fold folder ([], true) >> fst
 
-        let listRemoveFirst element =
-            let folder (xs, state) x = if state && x = element then xs, not state
-                                       else x :: xs, state
-            List.fold folder ([], true) >> fst
-
+    let private findAllJugglingRearrangements' inputSequence =
+        let (+~) = (ModuloP (List.length inputSequence)).AddModP ()
         let rec helper slotsTaken = function
             | []                -> Set.singleton []
             | inputSequenceTail -> let count = Set.count slotsTaken
@@ -27,18 +23,16 @@
                                                         |> helper (Set.add (a +~ count) slotsTaken)
                                                         |> Set.map (fun tail -> a::tail))
                                    |> Set.unionMany
-
-        member m.FindAllJugglingRearrangements =
-            match inputSequence with
-            | []      -> Set.empty
-            | n::ns   -> ns
-                         |> helper (Set.singleton n)
-                         |> Set.map (fun tail -> n::tail)
+        match inputSequence with
+        | []      -> Set.empty
+        | n::ns   -> ns
+                     |> helper (Set.singleton n)
+                     |> Set.map (fun tail -> n::tail)
 
     // Finds all rearrangements of a qualifying sequence to juggling sequences 
     // Remark: Not yet finished / fully tested
     let findAllJugglingRearrangements inputSequence =
         if qualifies inputSequence then
-            (QualifyingSequenceWithFindAllJugglingRearrangementsCapability inputSequence).FindAllJugglingRearrangements
+            findAllJugglingRearrangements' inputSequence
         else
             failwith "Input sequence does not qualify"
